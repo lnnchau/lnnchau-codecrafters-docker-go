bbfd8ded1fe75f91e655f72be6d64a604238ad81
Chau Le <nganchau.lenguyen@gmail.com>
2023-04-15 14:48:49 +0700

    stage 2

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+
 	// Uncomment this block to pass the first stage!
 	"os"
 	"os/exec"
@@ -9,20 +9,13 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage!
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
-	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-	fmt.Println(string(output))
+	cmd.Run()
 }
